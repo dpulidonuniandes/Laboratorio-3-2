6c3e1942a9d630c7640ea758da6ff8738acccbbc
JuanCamiloEduardo <69642359+JuanCamiloEduardo@users.noreply.github.com>
2022-10-02 13:20:44 -0500

    Server

--- Serve.py
+++ Serve.py
@@ -2,10 +2,10 @@ import socket
 import os
 from _thread import *
 
-IP = socket.gethostbyname(socket.gethostname())
+IP = '192.168.1.109'
 PORT = 4455
 ADDR = (IP, PORT)
-ThreadCount = 0
+ThreadCount = 1
 SIZE = 4096
 FORMAT = "utf-8"
 try:
@@ -22,34 +22,39 @@ except socket.error as e:
 
 def multi_threaded_client(conn,cliente):
     print("[LISTENING] Server is listening.")
-    while True:
-        """ Server has accepted the connection from the client. """
-        
-        print(f"[NEW CLIENT] {cliente} connected.")
+    """ Server has accepted the connection from the client. """
 
-        archivo="cliente" + str(cliente) + ".txt"        
-        """ Opening and reading the file data. """ 
-        filename= archivo #CAMBIAR POR UN INPUT
-        file = open(filename, "r")
-        data = file.read()
-        """ Sending the filename to the server. """
-        conn.send(filename.encode(FORMAT))
-        msg = conn.recv(SIZE).decode(FORMAT)
-        print(f"[SERVER]: {msg}")
-        """ Sending the file data to the server. """
-        conn.send(data.encode(FORMAT))
-        msg = conn.recv(SIZE).decode(FORMAT)
-        print(f"[SERVER]: {msg}")
-        """ Closing the file. """
-        file.close()
+    print(f"[NEW CLIENT] {cliente} connected.")
+
+    archivo="cliente" + str(cliente) + ".txt"        
+    """ Opening and reading the file data. """ 
+    filename= archivo #CAMBIAR POR UN INPUT
+    file = open(filename, "r")
+    data = file.read()
+    """ Sending the filename to the server. """
+    conn.send(filename.encode(FORMAT))
+    msg = conn.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+    """ Sending the file data to the server. """
+    conn.send(data.encode(FORMAT))
+    msg = conn.recv(SIZE).decode(FORMAT)
+    print(f"[SERVER]: {msg}")
+    """ Closing the file. """
+    file.close()
         
-        
+lista=[]
 while True:
-    Client, address = server.accept()
-    print('Connected to: ' + address[0] + ':' + str(address[1]))
-    ThreadCount += 1
+    if (ThreadCount<=5):
+        Client, address = server.accept()
+        cliente=[Client, address]
+        lista.append(cliente)
+        print('Connected to: ' + address[0] + ':' + str(address[1]))
     if(ThreadCount == 5):
-        start_new_thread(multi_threaded_client, (Client,ThreadCount ))
-    
+        for i in range(0,5):
+            multi_threaded_client(lista[i][0],i+1)
+ 
+    if (ThreadCount>5):
+        break
     print('Thread Number: ' + str(ThreadCount))
+    ThreadCount += 1
 server.close()
