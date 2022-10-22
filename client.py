010c8ef6bf10cb8c0563e8f56d547ec30420b07b
JuanCamiloEduardo <69642359+JuanCamiloEduardo@users.noreply.github.com>
2022-10-22 11:15:45 -0500

    Update client.py
    Cambios Cliente

--- client.py
+++ client.py
@@ -8,33 +8,38 @@ import hash
 import shutil
 
 def multiples(numero):
-    IP = '192.168.1.106'
-    PORT = 4455
+    IP = '192.168.1.109'
+    PORT = 12000
     ADDR = (IP, PORT)
     FORMAT = "utf-8"
     SIZE = 52428800
         
-    """ Staring a TCP socket. """
+    """ Staring a UDP socket. """
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
+    message = 'conectar'
     """ Connecting to the server. """
-    client.connect(ADDR)
     
-    
+    client.sendto(message.encode(),ADDR)
+    print('envio el mensaje')
+    modifiedMessage, serverAddress = client.recvfrom(SIZE)
+    print(" se ha hecho la conexio n con "+str(modifiedMessage))
     """ Receiving the filename from the server. """
-    filename = client.recv(SIZE).decode(FORMAT)
+    filename,serverAddress = client.recvfrom(SIZE)
+    filename=filename.decode()
     print("[RECV] Receiving the filename.")
     file = open(filename, "w")
-    client.send("Filename received.".encode(FORMAT))
+    client.sendto("Filename received.".encode(FORMAT),ADDR)
     
 
     """ Receiving the hash from the server. """
-    hashing=client.recv(SIZE).decode(FORMAT)
+    hashing,serverAddress =client.recvfrom(SIZE)
+    hashing=hashing.decode()
     print("Hash enviado \n")
     print(hashing)
     """ Receiving the filesize from the server. """
     
-    size = client.recv(SIZE).decode(FORMAT)
+    size,serverAddress =client.recvfrom(SIZE)
+    size=size.decode()
     size = size[0:9]
     filesize=0
     if("104857600"==size):
@@ -47,7 +52,7 @@ def multiples(numero):
     with open(filename, "wb") as f:
         while (file_stats.st_size!=filesize) :
             # read 1024 bytes from the socket (receive)
-            bytes_read = client.recv(SIZE)
+            bytes_read ,serverAddress =client.recvfrom(SIZE)
             
             if not bytes_read:    
                 
